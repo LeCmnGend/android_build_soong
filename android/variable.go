d794c033b4d1aeb9e249aa524a4a814a7e728e8a
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with Superior board flags
    Includes the following commit:
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Mon Sep 4 21:09:44 2017 +1000
    
        soong: explicitly specify name of Superior variables struct
    
        Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"superior/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -128,6 +130,9 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
+
+		// include Superior variables
+		Superior android.Product_variables
 
 		Flatten_apex struct {
 			Enabled *bool
@@ -351,6 +356,9 @@ type productVariables struct {
 	InstallExtraFlattenedApexes *bool `json:",omitempty"`
 
 	BoardUsesRecoveryAsBoot *bool `json:",omitempty"`
+
+	// include Superior variables
+	Superior android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
