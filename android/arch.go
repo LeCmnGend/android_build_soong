e011deb8fa8802f3db115f368623a6ffadbfb684
Tamas Petz <tamas.petz@arm.com>
2021-01-20 18:56:33 +0100

    [arm64] Build NDK with PAuth and BTI
    This change adds a new arch named "armv8-a-branchprot". The new target
    is no different from the usual "armv8-a" variant except it has the
    compiler flag "-mbranch-protection=standard" set.
    
    With this flag compiler emits Armv8.3-A Pointer Authentication and
    Armv8.5-A Branch Target Identification instructions. All of these
    instructions are in the hint space; therefore devices which do not
    support any of these extensions would execute them as NOPs.
    
    In terms of directory structure there is no change in the output of
    build/soong/scripts/build-ndk-prebuilts.sh script.
    
    Test: build NDK prebuilts
    
    Change-Id: Ie52ccba5653fbf567f0b7ca312d8a8e71602ad06

--- android/arch.go
+++ android/arch.go
@@ -1683,7 +1683,7 @@ func getMegaDeviceConfig() []archConfig {
 func getNdkAbisConfig() []archConfig {
 	return []archConfig{
 		{"arm", "armv7-a", "", []string{"armeabi-v7a"}},
-		{"arm64", "armv8-a", "", []string{"arm64-v8a"}},
+		{"arm64", "armv8-a-branchprot", "", []string{"arm64-v8a"}},
 		{"x86", "", "", []string{"x86"}},
 		{"x86_64", "", "", []string{"x86_64"}},
 	}
