ddccde9d67a7b78bede807a0607335e1a15bc35a
Akito Mizukito <akito@evolution-x.org>
2021-07-19 01:27:18 +0900

    soong: Whitelist resize2fs
    Change-Id: I549e0efb0b4df4f032f316b38e5b044461a2ef92
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -114,6 +114,7 @@ var Configuration = map[string]PathConfig{
 	"python3.10": Allowed,
 	"repo":    Allowed,
 	"rsync":   Allowed,
+	"resize2fs": Allowed,
 	"sh":      Allowed,
 	"tar":     Allowed,
 	"tr":      Allowed,
