bdf85efa95f0d84c5158d9a658ca3ca22b0315ba
Ido Ben-Hur <idoybh2@gmail.com>
2024-09-19 14:55:12 +0300

    soong: Installclean when TARGET_BUILD_GAPPS changes

--- ui/build/config.go
+++ ui/build/config.go
@@ -1106,6 +1106,16 @@ func (c *configImpl) BuildFromTextStub() bool {
 	return !c.buildFromSourceStub
 }
 
+func (c *configImpl) TargetBuildGapps() bool {
+	if v, ok := c.environ.Get("TARGET_BUILD_GAPPS"); ok {
+		v = strings.TrimSpace(v)
+		if v == "true" {
+			return true
+		}
+	}
+	return false
+}
+
 func (c *configImpl) TargetProduct() string {
 	if v, ok := c.environ.Get("TARGET_PRODUCT"); ok {
 		return v
@@ -1515,6 +1525,10 @@ func (c *configImpl) ProductOut() string {
 
 func (c *configImpl) DevicePreviousProductConfig() string {
 	return filepath.Join(c.ProductOut(), "previous_build_config.mk")
+}
+
+func (c *configImpl) DevicePreviousGappsConfig() string {
+	return filepath.Join(c.ProductOut(), "previous_gapps_config.mk")
 }
 
 func (c *configImpl) KatiPackageMkDir() string {
